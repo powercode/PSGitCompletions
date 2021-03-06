7ee7fc232ce522db598286fa5043e9fd4051f9d4
Filip Björklund <max.filip.bjorklund@gmail.com>
2021-03-06 13:01:19 +0100

    Added identity for GitRemote based on commit

--- src/PSGitCompletions/GitRemote.cs
+++ src/PSGitCompletions/GitRemote.cs
@@ -1,6 +1,8 @@
-﻿namespace PowerCode
+﻿using System;
+
+namespace PowerCode
 {
-    public struct GitRemote
+    public class GitRemote : IComparable<GitRemote>, IEquatable<GitRemote>, IComparable
     {
         public string Commit { get; }
         public string Name { get; }
@@ -9,6 +11,31 @@
         {
             Commit = commit;
             Name = name;
+        }
+
+        public int CompareTo(GitRemote? other)
+        {
+            if (ReferenceEquals(this, other))
+                return 0;
+            return other is null ? 1 : string.Compare(Commit, other.Commit, StringComparison.Ordinal);
+        }
+
+        public bool Equals(GitRemote? other)
+        {
+            if (other is null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return Commit == other.Commit;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return 0;
+            if (obj is null)
+                return 1;
+            return obj is GitRemote other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(GitRemote)}");
         }
     }
 }
